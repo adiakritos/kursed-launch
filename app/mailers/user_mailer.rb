509c684e4d9da6350d6507c3f1e90750f60b356b
Nick <adiakritos@gmail.com>
2015-11-10 13:19:40 -0800

    add gitignore, update user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
-    default from: "Kursed <hello@kursed.com>"
+  default from: "Kursed Apparel <info@kursedapparel.com>"
 
-    def signup_email(user)
-        @user = user
-        @twitter_message = ""
+  def signup_email(user)
+    @user = user
+    @twitter_message = "Stoked on this new @Kursedapparel collection! I’m going to be wearing #streetwear for free."
 
-        mail(:to => user.email, :subject => "Thanks for signing up!")
-    end
+    mail(:to => user.email, :subject => "Thanks for signing up!")
+  end
 end
