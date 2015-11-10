42514e3bc40f67c4058ad161eadaf6a722192d85
Nick <adiakritos@gmail.com>
2015-11-10 14:57:28 -0800

    update mailer info

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(:email => 'info@kursedappare.com', :password => '0YTFzhscAk9X', :password_confirmation => '0YTFzhscAk9X')
+AdminUser.create!(:email => 'adiakritos@gmail.com', :password => '0YTFzhscAk9X', :password_confirmation => '0YTFzhscAk9X')
